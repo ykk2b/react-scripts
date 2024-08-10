0117e1788ee4f8394b94d97bf59fdaba71f28b21
ykk2b <169088425+ykk2b@users.noreply.github.com>
2024-08-10 05:18:18 +0400

    add parcel

---
+++ README.md
@@ -0,0 +1,15 @@
+# React Scripts
+
+This repository contains collections of scripts used for initializing new React projects or adding new features to existing projects.
+
+## Scripts
+
+### Initialize
+
+- `./parce.sh <project_name>` - Initializes a new React project with Parcel bundler.
+
+todo: webpack, esbuild, swc, vite
+
+### Add
+
+todo: tailwindcss, jotai, react-query, react-router, react-markdown
---
+++ parcel.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+NAME=$1
+
+if [ -z "$NAME" ]; then
+  echo "please provide a name for the project"
+  exit 1
+fi
+
+if command -v pnpm >/dev/null 2>&1; then
+    package_manager="pnpm"
+    install_command="pnpm install"
+elif command -v yarn >/dev/null 2>&1; then
+    package_manager="yarn"
+    install_command="yarn add"
+elif command -v npm >/dev/null 2>&1; then
+    package_manager="npm"
+    install_command="npm install"
+else
+    package_manager="none"
+    install_command="echo 'No JavaScript package manager found.'"
+    exit 1
+fi
+
+echo "detected package manager: $package_manager"
+echo "starting project initialization..."
+
+if [ -d "$NAME" ]; then
+    echo "failed to initialize project: dir '$NAME' already exists."
+    exit 1
+fi
+mkdir "$NAME"
+cd "$NAME"
+
+package_json='{
+  "name": "'$NAME'",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.tsx",
+  "scripts": {
+    "start": "parcel serve index.html",
+    "build": "parcel build index.html"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "MIT",
+  "dependencies": {}
+}'
+
+echo "$package_json" > package.json
+
+dependencies=(
+  "react"
+  "react-dom"
+  "react-router-dom"
+)
+
+dev_dependencies=(
+  "@types/react"
+  "@types/react-dom"
+  "@types/react-router-dom"
+  "parcel"
+  "postcss"
+  "process"
+  "tailwindcss"
+  "typescript"
+)
+
+echo "installing dependencies..."
+
+$install_command ${dependencies[@]}
+$install_command -D ${dev_dependencies[@]}
+
+echo "dependencies installed."
+
+tailwind_config='
+ /** @type {import("tailwindcss").Config} */
+  export default {
+  content: ["./src/**/*.tsx", "./index.tsx", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}'
+
+echo "$tailwind_config" > tailwind.config.js
+
+tailwind_header='@tailwind base;
+@tailwind components;
+@tailwind utilities;'
+
+mkdir src
+mkdir src/styles
+echo "$tailwind_header" > src/styles/index.css
+
+postcss_config='{
+  "plugins": {
+    "tailwindcss": {}
+  }
+}'
+echo "$postcss_config" > .postcssrc
+
+echo "initialized tailwindcss"
+
+index_html='
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="title" content="'$NAME'" />
+        <meta name="description" content="'$NAME' description" />
+        <meta
+            name="keywords"
+            content=""
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="" />
+        <title>'$NAME'</title>
+    </head>
+    <body>
+        <div id="root"></div>
+    </body>
+    <script src="index.tsx" type="module"></script>
+</html>
+'
+echo "$index_html" > index.html
+
+index_tsx='import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./src/styles/index.css";
+import Home from "./src/pages/index";
+
+createRoot(document.getElementById("root")!).render(
+<React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+</React.StrictMode>
+);
+'
+
+echo "$index_tsx" > index.tsx
+
+home_tsx='import React from "react";
+
+export default function Home() {
+  return <div>Hello, World!</div>;
+}
+'
+
+mkdir src/pages
+echo "$home_tsx" > src/pages/index.tsx
+echo "project initialized."
